382562970018250a8aaab5d2d89b1955da702c13
Arnav <arnard76@gmail.com>
2025-07-31 17:46:54 +1200

    fix error page to show what the error was

--- UI/src/routes/+error.svelte
+++ UI/src/routes/+error.svelte
@@ -1,1 +1,11 @@
-ooops, page doesn't exist! <a href="/">Back to home page?</a>
+<script>
+	import { page } from '$app/state';
+</script>
+
+<h1>ooops, something went wrong!</h1>
+<h2>{page.status}: {page.error?.message}</h2>
+{#if page.error?.message.toLowerCase().includes('invalid token')}
+	<a href="/logout">Try logging out?</a>
+{:else}
+	<a href="/">Back to home page?</a>
+{/if}
